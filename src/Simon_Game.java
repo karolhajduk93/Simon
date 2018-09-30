75051d627d054a842661a3192fe669b5885f547d
Karol <karolhajduk93@gmail.com>
2018-09-30 23:08:39 +0200

    Generating pattern and checking it with user input

--- src/Simon_Game.java
+++ src/Simon_Game.java
@@ -3,10 +3,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Simon_Game extends JFrame implements ActionListener{
 
     static int buttonClicked = 0;
+    int patternCounter = -1;
+    ArrayList<Integer> pattern = new ArrayList<>();
+    ArrayList<Integer> move = new ArrayList<>();
+
     public static void main(String[] args) {
         new Simon_Game();
     }
@@ -24,6 +30,15 @@ public class Simon_Game extends JFrame implements ActionListener{
         this.add(draw);
         timer.start();
 
+
+        //tworzy pattern = 1
+        //dispalyuje pattern
+        //czeka az listener
+        //sprawdza czy takie same jak taki sam to dodaje do pattern i pattern = 2
+        // jak nie to reset i pattern = 1
+        //displayuje pattern
+        //czekaj na listener. jelis takie same i
+        createPattern();
         addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {}
@@ -32,15 +47,29 @@ public class Simon_Game extends JFrame implements ActionListener{
             public void mousePressed(MouseEvent e) {
                 if(e.getX() > 0 && e.getX() < 240 && e.getY() > 0 && e.getY() < 230){
                     buttonClicked = 1;
+                    move.add(1);
+                    patternCounter++;
+                    patternCheck();
+
                 }
                 else if(e.getX() > 240 && e.getX() < 500 && e.getY() > 0 && e.getY() < 230){
                     buttonClicked = 2;
+                    move.add(2);
+                    patternCounter++;
+                    patternCheck();
                 }
                 else if(e.getX() > 0 && e.getX() < 240 && e.getY() > 230 && e.getY() < 500){
                     buttonClicked = 3;
+                    move.add(3);
+                    patternCounter++;
+                    patternCheck();
                 }
                 else if(e.getX() > 240 && e.getX() < 500 && e.getY() > 230 && e.getY() < 500){
                     buttonClicked = 4;
+                    move.add(4);
+                    patternCounter++;
+                    patternCheck();
+
                 }
                 repaint();
 
@@ -64,5 +93,30 @@ public class Simon_Game extends JFrame implements ActionListener{
             buttonClicked = 0;
             repaint();
 
+    }
+
+    public void createPattern(){
+        Random random = new Random();
+        pattern.add(random.nextInt(4) + 1);
+        System.out.println(pattern.toString());
+    }
+
+    public void patternCheck(){
+        System.out.println(patternCounter + pattern.size());
+        if(move.get(patternCounter) == pattern.get(patternCounter)) {
+            if (patternCounter == pattern.size() - 1) {
+                createPattern();
+                //display pattern
+                move.clear();
+                patternCounter = -1;
+            }
+        }
+        else {
+            move.clear();
+            pattern.clear();
+            createPattern();
+            //display pattern
+            patternCounter = -1;
+        }
     }
 }
